eb3b4f6330b6ea09625f2440eb4ae5ce26792c0d
kartalayca98-oss <kartalayca98@gmail.com>
2026-08-27 15:39:43 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from config import Config
 from app.database import init_db
 
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static')
+    CORS(app)
     app.config.from_object(Config)
 
     # Veritabanını ilklendir
